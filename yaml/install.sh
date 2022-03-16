b588599b58a3886291c13361907010c3d596c081
root <chaejin_lee@tmax.co.kr>
2022-03-16 11:13:47 +0900

    [mod] install.sh 수정

--- yaml/install.sh
+++ yaml/install.sh
@@ -7,15 +7,13 @@ source ./opensearch.config
 
 echo "OS_VERSION = $OS_VERSION"
 echo "DASHBOARD_VERSION = $DASHBOARD_VERSION"
-echo "HYPERAUTH_URL = $HYPERAUTH_URL"
-echo "DASHBOARD_CLIENT_SECRET = $DASHBOARD_CLIENT_SECRET"
-echo "CUSTOM_DOMAIN_NAME = $CUSTOM_DOMAIN_NAME"
+#echo "CUSTOM_DOMAIN_NAME = $CUSTOM_DOMAIN_NAME"
 echo "FLUENTD_VERSION = $FLUENTD_VERSION"
 echo "BUSYBOX_VERSION = $BUSYBOX_VERSION"
 if [ $STORAGECLASS_NAME != "{STORAGECLASS_NAME}" ]; then
   echo "STORAGECLASS_NAME = $STORAGECLASS_NAME"
 else
-  export STORAGECLASS_NAME=
+  sed -i 's/storageClassName: {STORAGECLASS_NAME}//g' 01_opensearch.yaml
   echo "STORAGECLASS_NAME = default-storage-class"
 fi
 if [ $REGISTRY != "{REGISTRY}" ]; then
@@ -23,13 +21,10 @@ if [ $REGISTRY != "{REGISTRY}" ]; then
 fi
 
 sed -i 's/{BUSYBOX_VERSION}/'${BUSYBOX_VERSION}'/g' 01_opensearch.yaml
-sed -i 's/{OS_VERSION}/'${ES_VERSION}'/g' 01_opensearch.yaml
-sed -i 's/{HYPERAUTH_URL}/'${HYPERAUTH_URL}'/g' 01_opensearch.yaml
-sed -i 's/{STORAGECLASS_NAME}/'${STORAGECLASS_NAME}'/g' 01_opensearch.yaml
+sed -i 's/{OS_VERSION}/'${OS_VERSION}'/g' 01_opensearch.yaml
+#sed -i 's/{STORAGECLASS_NAME}/'${STORAGECLASS_NAME}'/g' 01_opensearch.yaml
 sed -i 's/{DASHBOARD_VERSION}/'${DASHBOARD_VERSION}'/g' 02_opensearch-dashboards.yaml
-sed -i 's/{HYPERAUTH_URL}/'${HYPERAUTH_URL}'/g' 02_opensearch-dashboards.yaml
-sed -i 's/{DASHBOARD_CLIENT_SECRET}/'${DASHBOARD_CLIENT_SECRET}'/g' 02_opensearch-dashboards.yaml
-sed -i 's/{CUSTOM_DOMAIN_NAME}/'${CUSTOM_DOMAIN_NAME}'/g' 02_opensearch-dashboards.yaml
+#sed -i 's/{CUSTOM_DOMAIN_NAME}/'${CUSTOM_DOMAIN_NAME}'/g' 02_opensearch-dashboards.yaml
 sed -i 's/{FLUENTD_VERSION}/'${FLUENTD_VERSION}'/g' 03_fluentd.yaml
 sed -i 's/{FLUENTD_VERSION}/'${FLUENTD_VERSION}'/g' 03_fluentd_cri-o.yaml
 
@@ -63,7 +58,7 @@ fi
 echo " "
 echo "---2. Wait until Opensearch starts up---"
 echo "It will take a couple of minutes"
-sleep 15s
+sleep 1m
 set +e
 export OS_IP=`kubectl get svc -n kube-logging | grep opensearch | tr -s ' ' | cut -d ' ' -f3`
 for ((i=0; i<11; i++))
@@ -95,7 +90,8 @@ if [ $suc != 0 ]; then
   kubectl delete -f 02_opensearch-dashboards.yaml
   exit 1
 else
-  echo "Dashboard pod running success" 
+  echo "Dashboard pod running success"
+  sleep 10s
 fi
 
 # 4. Install Fluentd
@@ -110,6 +106,7 @@ if [ $suc != 0 ]; then
   exit 1
 else
   echo "Fluentd running success"
+  sleep 10s
 fi
 
 # 5. Wait until Dashboard makes an index and alias normally
@@ -155,6 +152,7 @@ do
   fi
 done
 echo "Dashboard made an alias on Opensearch successfully"
+sleep 10s
 set -e
 
 # 6. Create default index 'logstash-*'
