6d93a48ea0150c73bfa039c07376fb260bcf4726
chaejin-lee <94825897+chaejin-lee@users.noreply.github.com>
2022-04-05 15:25:45 +0900

    [mod] rightsizing plugin disable 설정 추가

--- yaml/install.sh
+++ yaml/install.sh
@@ -19,7 +19,13 @@ if [ $RS_PLUGIN == "true" ]; then
   sed -i 's/#{RS_PLUGIN_VOLUMEMOUNT}/- name: install-plugin-volume \n          mountPath: \/plugins/g' 02_opensearch-dashboards.yaml
   sed -i 's/#{RS_PLUGIN_VOLUME}/- name: install-plugin-volume \n        emptyDir: {}/g' 02_opensearch-dashboards.yaml
   sed -i 's/#{RS_PLUGIN_SETTING}/ls \/plugins\/*.zip | while read file; do \/usr\/share\/opensearch-dashboards\/bin\/opensearch-dashboards-plugin install file:\/\/$file; done/g' 02_opensearch-dashboards.yaml
-  sed -i 's/#{RS_PLUGIN_INGRESS}/- host: rightsizing.{CUSTOM_DOMAIN_NAME} \n    http: \n      paths: \n      - backend: \n          service: \n            name: rightsizing-api-server-svc \n            port: \n              number: 8000 \n        path: \/ \n        pathType: Prefix/g' 02_opensearch-dashboards.yaml
+  sed -i 's/#{RS_PLUGIN_INGRESS}/- host: rightsizing.{CUSTOM_DOMAIN_NAME} \n    http: \n      paths: \n      - backend: \n          service: \n            name: rightsizing-api-server-svc \n            port: \n              number: 8000 \n        path: \/ \n        pathType: Prefix/#{RS_PLUGIN_INITCONTAINER}/g' 02_opensearch-dashboards.yaml
+else
+  sed -i 's/initContainers: \n      - name: install-plugins \n        image: docker.io\/tmaxcloudck\/rightsizing-opensearch-plugin:demo \n        command: ["sh", "-c", "cp -r \/workspace\/* \/plugins"] \n        volumeMounts: \n        - name: install-plugin-volume \n          mountPath: \/plugins/g' 02_opensearch-dashboards.yaml
+  sed -i 's/- name: install-plugin-volume \n          mountPath: \/plugins/#{RS_PLUGIN_VOLUMEMOUNT}/g' 02_opensearch-dashboards.yaml
+  sed -i 's/- name: install-plugin-volume \n        emptyDir: {}/#{RS_PLUGIN_VOLUME}/g' 02_opensearch-dashboards.yaml
+  sed -i 's/ls \/plugins\/*.zip | while read file; do \/usr\/share\/opensearch-dashboards\/bin\/opensearch-dashboards-plugin install file:\/\/$file; done/#{RS_PLUGIN_SETTING}/g' 02_opensearch-dashboards.yaml
+  sed -i 's/- host: rightsizing.{CUSTOM_DOMAIN_NAME} \n    http: \n      paths: \n      - backend: \n          service: \n            name: rightsizing-api-server-svc \n            port: \n              number: 8000 \n        path: \/ \n        pathType: Prefix/#{RS_PLUGIN_INGRESS}/g' 02_opensearch-dashboards.yaml
 fi
 if [ $STORAGECLASS_NAME != "{STORAGECLASS_NAME}" ]; then
   sed -i 's/{STORAGECLASS_NAME}/'${STORAGECLASS_NAME}'/g' 01_opensearch.yaml
