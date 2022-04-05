0733eb1fe2ac6f37c8038422368cd2ec5fccdf84
root <chaejin_lee@tmax.co.kr>
2022-04-05 14:21:41 +0900

    [mod] install.sh rightsizing 설정

--- yaml/install.sh
+++ yaml/install.sh
@@ -15,14 +15,16 @@ echo "FLUENTD_VERSION = $FLUENTD_VERSION"
 echo "BUSYBOX_VERSION = $BUSYBOX_VERSION"
 
 if [ $RS_PLUGIN == "true" ]; then
-  sed -i 's/#//g' 02_opensearch-dashboards.yaml
-  echo "Opensearch-Dashboards Rightsizing Plugin Enabled"
+  sed -i 's/#{RS_PLUGIN_INITCONTAINER}/initContainers: \n      - name: install-plugins \n        image: docker.io\/tmaxcloudck\/rightsizing-opensearch-plugin:demo \n        command: ["sh", "-c", "cp -r \/workspace\/* \/plugins"] \n        volumeMounts: \n        - name: install-plugin-volume \n          mountPath: \/plugins/g' 02_opensearch-dashboards.yaml
+  sed -i 's/#{RS_PLUGIN_VOLUMEMOUNT}/- name: install-plugin-volume \n          mountPath: \/plugins/g' 02_opensearch-dashboards.yaml
+  sed -i 's/#{RS_PLUGIN_VOLUME}/- name: install-plugin-volume \n        emptyDir: {}/g' 02_opensearch-dashboards.yaml
+  sed -i 's/#{RS_PLUGIN_SETTING}/ls \/plugins\/*.zip | while read file; do \/usr\/share\/opensearch-dashboards\/bin\/opensearch-dashboards-plugin install file:\/\/$file; done/g' 02_opensearch-dashboards.yaml
 fi
 if [ $STORAGECLASS_NAME != "{STORAGECLASS_NAME}" ]; then
   sed -i 's/{STORAGECLASS_NAME}/'${STORAGECLASS_NAME}'/g' 01_opensearch.yaml
   echo "STORAGECLASS_NAME = $STORAGECLASS_NAME"
 else
-  sed -i 's/storageClassName: {STORAGECLASS_NAME}//g' 01_opensearch.yaml
+  sed -e 's/storageClassName: {STORAGECLASS_NAME}//' 01_opensearch.yaml
   echo "STORAGECLASS_NAME = default-storage-class"
 fi
 if [ $REGISTRY != "{REGISTRY}" ]; then
@@ -48,162 +50,162 @@ if [ $REGISTRY != "{REGISTRY}" ]; then
 fi
 
 # 1. Install OpenSearch
-echo " "
-echo "---Installation Start---"
-kubectl create namespace kube-logging
-
-echo " "
-echo "---1. Install OpenSearch---"
-kubectl apply -f 01_opensearch.yaml
-timeout 5m kubectl -n kube-logging rollout status statefulset/os-cluster
-suc=`echo $?`
-if [ $suc != 0 ]; then
-  echo "Failed to install OpenSearch"
-  kubectl delete -f 01_opensearch.yaml
-  exit 1
-else
-  echo "OpenSearch pod running success" 
-fi
-
-# 2. Wait until Opensearch starts up
-echo " "
-echo "---2. Wait until Opensearch starts up---"
-echo "It will take a couple of minutes"
-sleep 1m
-set +e
-export OS_IP=`kubectl get svc -n kube-logging | grep opensearch | tr -s ' ' | cut -d ' ' -f3`
-for ((i=0; i<11; i++))
-do
-  curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/
-  is_success=`echo $?`
-  if [ $is_success == 0 ]; then
-    break
-  elif [ $i == 10 ]; then
-    echo "Timeout. Start uninstall"
-    kubectl delete -f 01_opensearch.yaml
-    exit 1
-  else
-    echo "try again..."
-    sleep 1m
-  fi
-done
-echo "OpenSearch starts up successfully"
-set -e
-
-# 3. Install Opensearch-Dashboards
-echo " "
-echo "---3. Install Opensearch-Dashboards---"
-kubectl apply -f 02_opensearch-dashboards.yaml
-timeout 5m kubectl -n kube-logging rollout status deployment/dashboard
-suc=`echo $?`
-if [ $suc != 0 ]; then
-  echo "Failed to install opensearch-dashboards"
-  kubectl delete -f 02_opensearch-dashboards.yaml
-  exit 1
-else
-  echo "Dashboard pod running success"
-  sleep 10s
-fi
-
-# 4. Install Fluentd
-echo " "
-echo "---4. Install Fluentd---"
-kubectl apply -f 03_fluentd_cri-o.yaml
-timeout 10m kubectl -n kube-logging rollout status daemonset/fluentd
-suc=`echo $?`
-if [ $suc != 0 ]; then
-  echo "Failed to install Fluentd"
-  kubectl delete -f 03_fluentd_cri-o.yaml
-  exit 1
-else
-  echo "Fluentd running success"
-  sleep 10s
-fi
-
-# 5. Wait until Dashboard makes an index and alias normally
-echo " "
-echo "---5. Wait until Dashboard makes an index and alias normally---"
-echo "It will take a couple of minutes"
-sleep 10s
-set +e
-for ((i=0; i<11; i++))
-do
-  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/`
-
-  if [[ "$is_success" == *".kibana"* ]]; then
-    break
-  elif [ $i == 10 ]; then
-    echo "Timeout. Failed to make an kibana index"
-    exit 1
-  else
-    echo "try again..."
-    sleep 1m
-  fi
-done
-echo "Dashboard made an index on Opensearch successfully"
-
-echo " "
-echo "---Wait until Dashboard makes an alias normally---"
-for ((i=0; i<11; i++))
-do
-  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_alias`
-  is_dashboard_normal=`kubectl get pod -n kube-logging | grep dashboard | tr -s ' ' | cut -d ' ' -f4`
-
-  if [[ "$is_success" == *".kibana_1"* ]]; then
-    break
-  elif [ $is_dashboard_normal != 0 ]; then
-    echo "make an index manually by curl command"
-    curl -XPUT -k -u admin:admin https://$OS_IP:9200/.kibana_1/_alias/.kibana
-  elif [ $i == 10 ]; then
-    echo "Timeout. Failed to make a alias for kibana index"
-    exit 1
-  else
-    echo "try again..."
-    sleep 1m
-  fi
-done
-echo "Dashboard made an alias on Opensearch successfully"
-sleep 10s
-set -e
-
-# 6. Create default index 'logstash-*'
-echo " "
-echo "---6. Create default index 'logstash-*'---"
-echo "It will take a couple of minutes"
-set +e
-export DASHBOARD_IP=`kubectl get svc -n kube-logging | grep dashboard | tr -s ' ' | cut -d ' ' -f3`
-
-for ((i=0; i<11; i++))
-do
-  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/`
-
-  if [[ "$is_success" == *"logstash"* ]]; then
-    break
-  elif [ $i == 10 ]; then
-    echo "Timeout. Failed to make a default index 'logstash-*'"
-    exit 1
-  else
-    echo "try again..."
-    sleep 1m
-  fi
-done
-echo "logstash index was made in OpenSearch"
-
-for ((i=0; i<11; i++))
-do
-  is_success=`curl -XGET -k -u admin:admin http://$DASHBOARD_IP:5601/api/status -I`
-
-  if [[ "$is_success" == *"200 OK"* ]]; then
-    break
-  elif [ $i == 10 ]; then
-    echo "Timeout. Dashboard status is not ready"
-    exit 1
-  else
-    echo "waiting for Dashboard starts up..."
-    sleep 1m
-  fi
-done
-echo "Dashboard starts up successfully"
+#echo " "
+#echo "---Installation Start---"
+#kubectl create namespace kube-logging
+#
+#echo " "
+#echo "---1. Install OpenSearch---"
+#kubectl apply -f 01_opensearch.yaml
+#timeout 5m kubectl -n kube-logging rollout status statefulset/os-cluster
+#suc=`echo $?`
+#if [ $suc != 0 ]; then
+#  echo "Failed to install OpenSearch"
+#  kubectl delete -f 01_opensearch.yaml
+#  exit 1
+#else
+#  echo "OpenSearch pod running success" 
+#fi
+#
+## 2. Wait until Opensearch starts up
+#echo " "
+#echo "---2. Wait until Opensearch starts up---"
+#echo "It will take a couple of minutes"
+#sleep 1m
+#set +e
+#export OS_IP=`kubectl get svc -n kube-logging | grep opensearch | tr -s ' ' | cut -d ' ' -f3`
+#for ((i=0; i<11; i++))
+#do
+#  curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/
+#  is_success=`echo $?`
+#  if [ $is_success == 0 ]; then
+#    break
+#  elif [ $i == 10 ]; then
+#    echo "Timeout. Start uninstall"
+#    kubectl delete -f 01_opensearch.yaml
+#    exit 1
+#  else
+#    echo "try again..."
+#    sleep 1m
+#  fi
+#done
+#echo "OpenSearch starts up successfully"
+#set -e
+#
+## 3. Install Opensearch-Dashboards
+#echo " "
+#echo "---3. Install Opensearch-Dashboards---"
+#kubectl apply -f 02_opensearch-dashboards.yaml
+#timeout 5m kubectl -n kube-logging rollout status deployment/dashboard
+#suc=`echo $?`
+#if [ $suc != 0 ]; then
+#  echo "Failed to install opensearch-dashboards"
+#  kubectl delete -f 02_opensearch-dashboards.yaml
+#  exit 1
+#else
+#  echo "Dashboard pod running success"
+#  sleep 10s
+#fi
+#
+## 4. Install Fluentd
+#echo " "
+#echo "---4. Install Fluentd---"
+#kubectl apply -f 03_fluentd_cri-o.yaml
+#timeout 10m kubectl -n kube-logging rollout status daemonset/fluentd
+#suc=`echo $?`
+#if [ $suc != 0 ]; then
+#  echo "Failed to install Fluentd"
+#  kubectl delete -f 03_fluentd_cri-o.yaml
+#  exit 1
+#else
+#  echo "Fluentd running success"
+#  sleep 10s
+#fi
+#
+## 5. Wait until Dashboard makes an index and alias normally
+#echo " "
+#echo "---5. Wait until Dashboard makes an index and alias normally---"
+#echo "It will take a couple of minutes"
+#sleep 10s
+#set +e
+#for ((i=0; i<11; i++))
+#do
+#  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/`
+#
+#  if [[ "$is_success" == *".kibana"* ]]; then
+#    break
+#  elif [ $i == 10 ]; then
+#    echo "Timeout. Failed to make an kibana index"
+#    exit 1
+#  else
+#    echo "try again..."
+#    sleep 1m
+#  fi
+#done
+#echo "Dashboard made an index on Opensearch successfully"
+#
+#echo " "
+#echo "---Wait until Dashboard makes an alias normally---"
+#for ((i=0; i<11; i++))
+#do
+#  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_alias`
+#  is_dashboard_normal=`kubectl get pod -n kube-logging | grep dashboard | tr -s ' ' | cut -d ' ' -f4`
+#
+#  if [[ "$is_success" == *".kibana_1"* ]]; then
+#    break
+#  elif [ $is_dashboard_normal != 0 ]; then
+#    echo "make an index manually by curl command"
+#    curl -XPUT -k -u admin:admin https://$OS_IP:9200/.kibana_1/_alias/.kibana
+#  elif [ $i == 10 ]; then
+#    echo "Timeout. Failed to make a alias for kibana index"
+#    exit 1
+#  else
+#    echo "try again..."
+#    sleep 1m
+#  fi
+#done
+#echo "Dashboard made an alias on Opensearch successfully"
+#sleep 10s
+#set -e
+#
+## 6. Create default index 'logstash-*'
+#echo " "
+#echo "---6. Create default index 'logstash-*'---"
+#echo "It will take a couple of minutes"
+#set +e
+#export DASHBOARD_IP=`kubectl get svc -n kube-logging | grep dashboard | tr -s ' ' | cut -d ' ' -f3`
+#
+#for ((i=0; i<11; i++))
+#do
+#  is_success=`curl -XGET -k -u admin:admin https://$OS_IP:9200/_cat/indices/`
+#
+#  if [[ "$is_success" == *"logstash"* ]]; then
+#    break
+#  elif [ $i == 10 ]; then
+#    echo "Timeout. Failed to make a default index 'logstash-*'"
+#    exit 1
+#  else
+#    echo "try again..."
+#    sleep 1m
+#  fi
+#done
+#echo "logstash index was made in OpenSearch"
+#
+#for ((i=0; i<11; i++))
+#do
+#  is_success=`curl -XGET -k -u admin:admin http://$DASHBOARD_IP:5601/api/status -I`
+#
+#  if [[ "$is_success" == *"200 OK"* ]]; then
+#    break
+#  elif [ $i == 10 ]; then
+#    echo "Timeout. Dashboard status is not ready"
+#    exit 1
+#  else
+#    echo "waiting for Dashboard starts up..."
+#    sleep 1m
+#  fi
+#done
+#echo "Dashboard starts up successfully"
 echo " "
 echo "---Installation Done---"
 popd
